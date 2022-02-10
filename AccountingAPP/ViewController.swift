9343f83b68c3da65a2833d7e2d3bc53ff126b23e
YongShun <a49980060@gmail.com>
2022-02-11 01:28:07 +0800

    Initial Commit

---
+++ AccountingAPP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AccountingAPP
+//
+//  Created by 塗詠順 on 2022/2/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
